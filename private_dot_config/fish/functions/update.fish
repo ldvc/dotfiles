4c5270a48945a8788f61745a44052983cadeab30
Ludovic Terrier <ludovic@terrier.im>
2025-12-19 00:14:03 +0100

    chore(fish): run fisher update

--- private_dot_config/fish/functions/update.fish
+++ private_dot_config/fish/functions/update.fish
@@ -1,10 +1,10 @@
 function update -d "update brew, fish, fisher and mac app store"
     echo 'start updating ...'
 
-    echo 'updating homebrew'
-    brew update
-    brew upgrade
-    brew cleanup
+    #echo 'updating homebrew'
+    #brew update
+    #brew upgrade
+    #brew cleanup
 
     echo 'updating fish shell'
     fisher update
